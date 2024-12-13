c5aa45f79e6f7f1fd4ca6a825fd36ea0340507da
Yevheniia Cherepashchuk <chtozalogin.jh@gmail.com>
2024-12-13 15:59:57 +0100

    fix 4 details

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,13 +1,9 @@
 export const errorHandler = (error, req, res, next) => {
-  const {
-    status = 500,
-    message = 'Something went wrong',
-    details = [],
-  } = error;
+  const { status = 500, message = 'Something went wrong', details } = error;
 
   res.status(status).json({
     status,
     message,
-    details,
+    details: details || [], // Якщо є деталі, то їх передаємо
   });
 };
--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -1,3 +1,14 @@
+// import createHttpError from 'http-errors';
+
+// export const validateBody = (schema) => async (req, res, next) => {
+//   try {
+//     await schema.validateAsync(req.body, { abortEarly: false });
+//     next();
+//   } catch (err) {
+//     const error = createHttpError(400, 'Bad Request', { error: err.details });
+//     next(error);
+//   }
+// };
 import createHttpError from 'http-errors';
 
 export const validateBody = (schema) => async (req, res, next) => {
@@ -5,7 +16,16 @@ export const validateBody = (schema) => async (req, res, next) => {
     await schema.validateAsync(req.body, { abortEarly: false });
     next();
   } catch (err) {
-    const error = createHttpError(400, 'Bad Request', { error: err.details });
-    next(error);
+    // Перевіряємо на наявність помилки Joi
+    if (err.isJoi) {
+      const details = err.details.map((detail) => ({
+        message: detail.message,
+        path: detail.path,
+      }));
+      const error = createHttpError(400, 'Bad Request', { details });
+      next(error);
+    } else {
+      next(err); // Для інших помилок
+    }
   }
 };
